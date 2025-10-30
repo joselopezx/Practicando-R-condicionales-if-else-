344706e893f56b6abb8272073b822686bb5ec84d
joselopezx <jose0xlopez@gmail.com>
2025-10-29 21:49:13 -0600

    Add files via upload

--- Calculando el tiempo total del proyecto.R
+++ Calculando el tiempo total del proyecto.R
@@ -4,16 +4,10 @@
 #de días negativo, el código debe avisar que los valores 
 #ingresados son inválidos y no calcular el total.
 
-actividadA <- -12
+actividadA <- 12
 actividadB <- 3
 actividadC <- 2
 
-if(actividadA<0){
-  print("No se aceptan numeros negativos : Actividad A")
-}
-if(actividadB<0){
-  print("No se aceptan numeros negativos : Actividad B")
-}
-if(actividadC<0){
-  print("No se aceptan numeros negativos : Actividad C")
+if(actividadA<0 || actividadB<0 || actividadC<0){
+  print("No se aceptan numeros negativos")
 }
